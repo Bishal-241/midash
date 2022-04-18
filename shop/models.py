e8e34428121572496eb42c6263f602a63b9ea6b0
Bishal Shah <anjalishahjhau12345@gmail.com>
2022-04-18 19:20:16 +0545

    Models added and fieldsetup

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+# from pytz import timezone
+from django.utils.timezone import datetime
 
 # Create your models here.
+class Product(models.Model):
+    pId = models.AutoField
+    pName = models.CharField(max_length=50)
+    pClass = models.IntegerField()
+    pDate = models.DateField(default=datetime.now)
+    
+    pDesc = models.CharField(max_length=300)
+    
+
+
